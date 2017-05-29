6d35203b05fca22a19cf5edc792e48f6f86fef15
William Button IV <theallpro727@gmail.com>
2017-05-29 17:06:21 -0400

    Update phone-list.controller.js

--- week6/lab/js/phone-list.controller.js
+++ week6/lab/js/phone-list.controller.js
@@ -2,14 +2,16 @@
 
 (function () 
 {
+   //below refrences the phone-list-controller file
     'use strict';
     angular
             .module('app')
             .controller('PhoneListController', PhoneListController);
-
+    
     PhoneListController.$inject = ['PhonesService'];
-
-
+    //ajax call, returns data as a promise
+    //sets view model for phones to that data
+    //allows auto-update while page is running
     function PhoneListController(PhonesService) 
     {
         var vm = this;
