61bc7c72ae6fba1047a43e2e9a5bfeecabed75ab
William Button IV <theallpro727@gmail.com>
2017-05-29 16:46:36 -0400

    Update constant.js

--- week6/lab/js/constant.js
+++ week6/lab/js/constant.js
@@ -1,7 +1,7 @@
 (function () 
 {
     'use strict';
-
+    //here we add a url reference to the json files for where index.html will read the files
     angular
             .module('app')
             .constant('REQUEST', {
