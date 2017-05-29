59c90234267e5b51161fff842ae317c700d32112
William Button IV <theallpro727@gmail.com>
2017-05-29 16:45:35 -0400

    Update app.js

--- week6/lab/js/app.js
+++ week6/lab/js/app.js
@@ -13,6 +13,7 @@
     function config($routeProvider) 
     {
         $routeProvider.
+        //"when" is used for directing modules to the js and html templates, a kind of if/else if arrangement
                 when('/', 
                 {
                     templateUrl: 'js/phone-list.template.html',
